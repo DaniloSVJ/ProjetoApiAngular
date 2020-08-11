b7f4b88b66218022afc6378f6f094d628eccdac8
DaniloSVJ <danilocotatoceu@gmail.com>
2020-08-11 19:38:45 -0300

    Api Angular

---
+++ ClientApp/app/components/pessoas/pessoas.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { IPessoa } from './pessoa';
+import { PessoasService } from './pessoa.services';
+
+@Component({
+    selector: 'pessoas',
+    templateUrl: './pessoas.component.html'
+})
+export class PessoasComponent implements OnInit {
+    pessoas: IPessoa[];
+
+    constructor(private pessoaService: PessoasService) {
+
+    }
+
+    ngOnInit() {
+        this.pessoaService.getPessoas().subscribe(pes => this.pessoas = pes)
+    }
+}
